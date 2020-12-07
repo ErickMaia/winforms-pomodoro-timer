325adf661e55f90206dee0c9ae3b36098f32aeab
ErickMaia <erickmaiatmariano@gmail.com>
2020-12-06 21:43:08 -0300

    Removing maximum time spans from Pomodoro Class

--- PomodoroTimer/Entities/PomodoroTimer.cs
+++ PomodoroTimer/Entities/PomodoroTimer.cs
@@ -13,37 +13,27 @@ namespace Pomodoro.Entities
         private static TimeSpan _pomodoroTimeSpan;
         private static TimeSpan _shortBreakTimeSpan;
         private static TimeSpan _longBreakTimeSpan;
-        public static TimeSpan PomodoroTimeSpan {
+        public static TimeSpan PomodoroTimeSpan
+        {
             get
             {
                 return _pomodoroTimeSpan;
             }
-            set { 
-                if (value.TotalMinutes > 50)
-                {
-                    throw new DomainException("Maximum pomodoro time span: 50 minutes");
-                }
-                else
-                {
-                    _pomodoroTimeSpan = new TimeSpan(value.Ticks);
-                }
-            } 
+            set
+            {
+                _pomodoroTimeSpan = new TimeSpan(value.Ticks);
+            }
         }
         public static TimeSpan ShortBreakTimeSpan
         {
-            get {
+            get
+            {
                 return _shortBreakTimeSpan;
             }
 
-            set {
-                if (value.TotalMinutes > 10)
-                {
-                    throw new DomainException("Maximum short break time span: 10 minutes");
-                }
-                else
-                {
-                    _shortBreakTimeSpan = new TimeSpan(value.Ticks);
-                }
+            set
+            {
+                _shortBreakTimeSpan = new TimeSpan(value.Ticks);
             }
         }
         public static TimeSpan LongBreakTimeSpan
@@ -55,14 +45,7 @@ namespace Pomodoro.Entities
 
             set
             {
-                if (value.TotalMinutes > 30)
-                {
-                    throw new DomainException("Maximum long break time span: 30 minutes");
-                }
-                else
-                {
-                    _longBreakTimeSpan = new TimeSpan(value.Ticks);
-                }
+                _longBreakTimeSpan = new TimeSpan(value.Ticks);
             }
         }
         public static PomodoroStatus Status { get; set; } = PomodoroStatus.Stopped;
@@ -75,6 +58,6 @@ namespace Pomodoro.Entities
             TimeCounting = new TimeSpan(PomodoroTimeSpan.Ticks);
         }
 
-       
+
     }
 }
